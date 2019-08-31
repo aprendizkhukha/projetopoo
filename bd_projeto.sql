dc6bc0a24f282e2025a226652cf05fd90bbb965d
aprendizkhukha <54728470+aprendizkhukha@users.noreply.github.com>
2019-08-30 20:15:56 -0400

    Add files via upload

---
+++ bd_projeto.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 4.5.1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: 31-Ago-2019 às 02:23
+-- Versão do servidor: 5.7.25-log
+-- PHP Version: 5.6.21
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `bd_projeto`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `pessoa`
+--
+
+CREATE TABLE `pessoa` (
+  `id` int(11) UNSIGNED NOT NULL,
+  `nome` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
+  `endereco` varchar(80) COLLATE utf8_unicode_ci NOT NULL,
+  `telefone` varchar(35) COLLATE utf8_unicode_ci NOT NULL,
+  `email` varchar(40) COLLATE utf8_unicode_ci NOT NULL,
+  `sexo` varchar(1) COLLATE utf8_unicode_ci NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
+
+--
+-- Extraindo dados da tabela `pessoa`
+--
+
+INSERT INTO `pessoa` (`id`, `nome`, `endereco`, `telefone`, `email`, `sexo`) VALUES
+(1, 'Samuel Castro', 'rua flavio', '9299209', 'samuel@samuel.com.br', 'M'),
+(2, 'Thiago Vinicius', 'rua boa', '000000000', 'thiago@thiago.com.br', 'M'),
+(3, 'Adriana Lima', 'rua boa', '9299133', 'adriana@adriana.com.br', 'F'),
+(4, 'Sonia Maria', 'rua flavio', '9299113', 'sonia@sonia.com.br', 'F'),
+(5, 'Raimundo Rocha', 'rua flavio', '91990000', 'raimundo@raimundo.com.br', 'M');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `pessoa`
+--
+ALTER TABLE `pessoa`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `pessoa`
+--
+ALTER TABLE `pessoa`
+  MODIFY `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
